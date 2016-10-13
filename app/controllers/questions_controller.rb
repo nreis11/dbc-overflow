9633c86b21f5135a3be1894135039828c518f3de
Frank Sommers & Alex Carroll & Nick Reis & Ali Baker <pair+ffsommers.lexac1.reeses-pieces.thealicat13@devbootcamp.com>
2016-10-13 15:56:33 -0700

    Add routes and erb views to question controller. TODO: Active record calls and erb code logic in the controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,1 +1,32 @@
-a
+get '/questions/?' do
+  erb :'questions/index'
+end
+
+post '/questions/?' do
+  # new question logic here
+  redirect "/questions/#{@question.id}"
+end
+
+get '/questions/new/?' do
+
+  erb :'questions/new'
+end
+
+get '/questions/:id/?' do
+
+  erb :'questions/show'
+end
+
+get '/questions/:id/edit/?' do
+
+  erb :'questions/edit'
+end
+
+put '/questions/:id/?' do
+  redirect "/questions/#{@question.id}"
+end
+
+delete '/questions/:id' do
+  # destroy question in AR
+  redirect '/questions'
+end
